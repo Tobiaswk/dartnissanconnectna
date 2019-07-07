162d1ce5d46d1f5a46cba8c8f2fbec06b5f259c4
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2019-07-07 17:14:50 +0200

    Add missing exports

--- lib/dartnissanconnectna.dart
+++ lib/dartnissanconnectna.dart
@@ -3,3 +3,6 @@ library dartnissanconnectna;
 export 'src/nissanconnect_session.dart';
 export 'src/nissanconnect_vehicle.dart';
 export 'src/nissanconnect_battery.dart';
+export 'src/nissanconnect_location.dart';
+export 'src/nissanconnect_stats.dart';
+export 'src/nissanconnect_trips.dart';
